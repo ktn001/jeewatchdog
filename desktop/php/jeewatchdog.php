d14454477cb7717313ff66df2a736ab7cc45523b
Votre Nom <Vous@exemple.com>
2026-09-13 22:08:47 +0200

    Affichage du logicalId des commandes

--- desktop/php/jeewatchdog.php
+++ desktop/php/jeewatchdog.php
@@ -232,6 +232,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
 								<th class="hidden-xs" style="min-width:50px;width:70px;">ID</th>
 								<th style="min-width:200px;width:350px;">{{Nom}}</th>
 								<th style="width:130px;">{{Type}}</th>
+								<th style="width:130px;">LogicalId</th>
 								<th style="min-width:260px;">{{Options}}</th>
 								<th>{{Etat}}</th>
 								<th style="min-width:80px;width:200px;">{{Actions}}</th>
